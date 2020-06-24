9c5e0d71e04b9ea80e8671904163c8f6dc5c983c
erikagarcia29 <erikagarciasierra@gmail.com>
2020-06-24 00:45:16 -0300

    se crea put actualizar sueldo empleados y borrar empleado  en controler y service ,se creo la clase saldoInfoRequest

--- rrhh/src/main/java/ar/com/ada/api/rrhh/controllers/EmpleadoControler.java
+++ rrhh/src/main/java/ar/com/ada/api/rrhh/controllers/EmpleadoControler.java
@@ -3,13 +3,18 @@ package ar.com.ada.api.rrhh.controllers;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.ada.api.rrhh.entities.Empleado;
+import ar.com.ada.api.rrhh.models.requests.SueldoInfoRequest;
 import ar.com.ada.api.rrhh.models.requests.infoBasicaEmpleadoRequest;
 import ar.com.ada.api.rrhh.models.responses.GenericResponse;
 import ar.com.ada.api.rrhh.services.CategoriaService;
@@ -50,4 +55,42 @@ public class EmpleadoControler {
 
   }
 
+  @PutMapping("/empleados/{id}/sueldos")
+  public ResponseEntity<?> actualizarSueldoEmpleado(@PathVariable int id, @RequestBody SueldoInfoRequest infoNueva) {
+
+    Empleado empleadoOriginal = empleadoService.traerEmpleadoPorId(id);
+
+    if (empleadoOriginal != null) {
+
+      empleadoService.actualizarSueldoEmpleado(empleadoOriginal, infoNueva.sueldoNuevo);
+
+      GenericResponse resp = new GenericResponse();
+      resp.isOk = true;
+      resp.id = empleadoOriginal.getEmpleadoId();
+      resp.message = "Se ha actualizado con exito";
+
+      return ResponseEntity.ok(resp);
+    }
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+  @DeleteMapping("/empleados/{id}")
+  public ResponseEntity<?> borrarEmpleado(@PathVariable int id) {
+
+    Empleado empleado = empleadoService.traerEmpleadoPorId(id);
+
+    if (empleado != null) {
+
+      empleadoService.borrarEmpleado(empleado);
+
+      GenericResponse resp = new GenericResponse();
+      resp.isOk = true;
+      resp.id = empleado.getEmpleadoId();
+      resp.message = "Fue eliminado con exito";
+
+      return ResponseEntity.ok(resp);
+    }
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+  }
 }
---
+++ rrhh/src/main/java/ar/com/ada/api/rrhh/models/requests/SueldoInfoRequest.java
@@ -0,0 +1,8 @@
+package ar.com.ada.api.rrhh.models.requests;
+
+import java.math.BigDecimal;
+
+public class SueldoInfoRequest {
+   
+        public BigDecimal sueldoNuevo;
+}
